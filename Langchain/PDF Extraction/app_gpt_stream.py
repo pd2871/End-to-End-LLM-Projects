a8f49196fb6d1da3a8868d6aecfdf670a65d08c4
pd2871 <pdahal2871@gmail.com>
2024-03-28 17:53:04 +0545

    Added memory/chat-history

--- Langchain/PDF Extraction/app_gpt_stream.py
+++ Langchain/PDF Extraction/app_gpt_stream.py
@@ -7,11 +7,11 @@ import warnings, tiktoken
 warnings.filterwarnings("ignore")
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 openai.api_key = openai_api_key
@@ -32,11 +32,9 @@ def tiktoken_len(text):
 def create_retriever_chain(pdf_path):
     pdf_reader = PdfReader(pdf_path)
     text = ""
-    t1 = timer()
     for page in pdf_reader.pages:
         text += page.extract_text()
     # split into chunks
-    t1 = timer()
     text_splitter = RecursiveCharacterTextSplitter(
     separators=["\n\n", "\n", " ", ""],
     chunk_size=2000,
@@ -45,9 +43,35 @@ def create_retriever_chain(pdf_path):
     )
     chunks = text_splitter.split_text(text)
     
+    template = """
+                Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question:
+                ------
+                <ctx>
+                {context}
+                </ctx>
+                ------
+                <hs>
+                {history}
+                </hs>
+                ------
+                {question}
+                Answer:
+                """
+    prompt = PromptTemplate(
+        input_variables=["history", "context", "question"],
+        template=template,
+    )
+    
     # Index documents into search engine
     embeddings = OpenAIEmbeddings()
     vector_db = FAISS.from_texts(chunks, embeddings)
+    
+    memory = ConversationBufferWindowMemory(
+                                            memory_key='history',
+                                            input_key='question',
+                                            k=1,
+                                            return_messages=True
+                                            )
     
     # RAG Chain
     retriever = vector_db.as_retriever()
@@ -58,13 +82,17 @@ def create_retriever_chain(pdf_path):
                     model=model_name, temperature=0
     )
 
-    chain = RetrievalQA.from_chain_type(llm=llm,
-                                        retriever=retriever)
+    chain = RetrievalQA.from_chain_type(llm=llm, verbose=False,
+                                        retriever=retriever, chain_type_kwargs={
+                                                                            "verbose": False,
+                                                                            "prompt": prompt,
+                                                                            "memory": memory
+                                                                            })
     return chain
 
 
 def main(chain, prompt):
-    response = chain.invoke(prompt)
+    response = chain.invoke(prompt)  #get response from GPT
     print("Assistant: ", response['result'])
     print()
     print()
