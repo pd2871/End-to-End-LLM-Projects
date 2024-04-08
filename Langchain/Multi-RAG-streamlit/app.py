b5c6fd519f5a8770345a75d84e0d31aa0228b21c
pd2871 <pdahal2871@gmail.com>
2024-04-08 21:15:06 +0545

    updated files

---
+++ Langchain/Multi-RAG-streamlit/app.py
@@ -0,0 +1,59 @@
+
+from chains import Chain
+import shutil
+import streamlit as st
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain_core.messages import HumanMessage, AIMessage
+
+# def ask_question(chain, query):
+#     result = chain.invoke(query)
+#     return result["result"]
+
+def ask_question(chain, query):
+    return chain.stream(query)
+
+st.set_page_config(page_title='RAG - Q&A Bot', page_icon='🔗')
+st.title("Chat with PDF, Excel or CSV - 📚🚀")
+
+openai_api_key = st.sidebar.text_input("Enter your OpenAI GPT-4 API key", placeholder='OpenAI GPT-4 API Key...')
+file = st.sidebar.file_uploader("Upload File")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+    
+#write history
+for message in st.session_state['chat_history']:
+    if isinstance(message, HumanMessage):
+        with st.chat_message('user'):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message('assistant'):
+            st.markdown(message.content)
+    
+if openai_api_key and file:
+    if 'memory' not in st.session_state:
+        st.session_state['memory'] = ConversationBufferWindowMemory(
+                                            memory_key='history',
+                                            input_key='question',
+                                            k=1,
+                                            return_messages=True
+                                            )
+    # st.session_state['file'] = file
+    chain = Chain(file, openai_api_key, st.session_state['memory'])
+    rag_chain = chain.create_chain()
+    if rag_chain is None:
+        st.sidebar.error('Error: Upload only PDF, Excel or CSV file', icon="🚨")   
+    else:
+        with st.chat_message("assistant"):
+            st.write("Hello ! Ask me about your uploaded files 🤗")
+            
+        prompt = st.chat_input("Ask about your PDF, CSV or Excel data 🧮")
+        if prompt:
+            st.session_state['chat_history'].append(HumanMessage(prompt))
+            with st.chat_message("user"):
+                st.markdown(prompt)
+                
+            with st.chat_message("assistant"):
+                ai_response = st.write_stream(ask_question(rag_chain, prompt))
+            st.session_state['chat_history'].append(AIMessage(ai_response))
+    
--- Langchain/Multi-RAG-streamlit/chains.py
+++ Langchain/Multi-RAG-streamlit/chains.py
@@ -1,26 +1,30 @@
 
-from credentials import openai_api_key, model_name
-import os, openai
-import warnings, tiktoken, tempfile
+from credentials import model_name
+import os, openai, shutil
+import warnings, tempfile
 warnings.filterwarnings("ignore")
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.document_loaders import PyMuPDFLoader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import UnstructuredExcelLoader
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain_community.document_loaders.csv_loader import UnstructuredCSVLoader, UnstructuredExcelLoader
+from langchain_community.document_loaders.csv_loader import UnstructuredCSVLoader
 
-openai.api_key = openai_api_key
-os.environ['openai_api_key'] = openai_api_key
 
 
 class Chain:
-    def __init__(self, file_path):
+    def __init__(self, file_path, openai_api_key, memory):
+        self.openai_api_key = openai_api_key
+        openai.api_key = openai_api_key
+        os.environ['openai_api_key'] = openai_api_key
         self.file_path = file_path
+        self.file_name = file_path.name
+        self.memory = memory
+        self.embeddings = OpenAIEmbeddings()
         self.template = """
                         Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question:
                         ------
@@ -35,59 +39,52 @@ class Chain:
                         {question}
                         Answer:
                         """
+        self.prompt = PromptTemplate(
+                            input_variables=["history", "context", "question"],
+                            template=self.template,
+                            )
         
-    def pdf_loader(self):
-        with tempfile.NamedTemporaryFile() as tmp_file:
-            tmp_file.write(self.file_path.getvalue())
-            tmp_file_path = tmp_file.name
-
+    def pdf_loader(self, tmp_file_path):
         loader = PyMuPDFLoader(file_path=tmp_file_path)
         data = loader.load()
         return data
     
-    def csv_excel_loader(self):
-        if self.file_path.endswith('.csv'):
+    def csv_excel_loader(self, tmp_file_path):
+        if tmp_file_path.endswith('.csv'):
             loader = UnstructuredCSVLoader(file_path=self.file_path, mode="elements")
             data = loader.load()
-        elif self.file_path.endswith('.xlsx'):
+        elif tmp_file_path.endswith('.xlsx'):
             loader = UnstructuredExcelLoader(file_path=self.file_path, mode="elements")
             data = loader.load()
         return data
     
     def create_chain(self):
-        if self.file_path.endswith('.pdf'):
-            data = self.pdf_loader()
-        elif self.file_path.endswith('.csv') or self.file_path.endswith('.xlsx'):
-            data = self.csv_excel_loader()
-        else:
-            return None
-        #memory for chat history
-        memory = ConversationBufferWindowMemory(
-                                            memory_key='history',
-                                            input_key='question',
-                                            k=1,
-                                            return_messages=True
-                                            )
-        prompt = PromptTemplate(
-                            input_variables=["history", "context", "question"],
-                            template=self.template,
-                            )
-        # Create embeddings using Sentence Transformers
-        embeddings = OpenAIEmbeddings()
+        with tempfile.TemporaryDirectory()(dir='.', suffix='.'+self.file_name.split('.')[-1], mode='wb') as tmp_file:
+            with open(f'files/temp.file', 'w') as f:
+                f.write('success')
+            tmp_file.write(self.file_path.getvalue())
+            tmp_file_path = tmp_file.name
+            if self.file_name.endswith('.pdf'):
+                data = self.pdf_loader(tmp_file_path)
+            elif tmp_file_path.endswith('.csv') or tmp_file_path.endswith('.xlsx'):
+                data = self.csv_excel_loader(tmp_file_path)
+            else:
+                return None
+        
         # Create a FAISS vector store and save embeddings
-        retriever = FAISS.from_documents(data, embeddings).as_retriever()
-        # Load the language model
+        retriever = FAISS.from_documents(data, self.embeddings).as_retriever()
+        # Load the LLM
         llm = ChatOpenAI(streaming=True,
                     callbacks=[StreamingStdOutCallbackHandler()],
-                    openai_api_key=openai_api_key,
+                    openai_api_key=self.openai_api_key,
                     model=model_name, temperature=0
                     )
         # Create a conversational chain
         chain = RetrievalQA.from_chain_type(llm=llm, verbose=False,
                                         retriever=retriever, chain_type_kwargs={
                                                                             "verbose": False,
-                                                                            "prompt": prompt,
-                                                                            "memory": memory
+                                                                            "prompt": self.prompt,
+                                                                            "memory": self.memory
                                                                             })
         return chain
     
